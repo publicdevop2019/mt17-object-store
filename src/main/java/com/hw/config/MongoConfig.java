cf018aee1f4bbdea20c0e7306148febcfe63d9a4
publicdevop2019 <publicdevop2019hw@gmail.com>
2020-11-28 00:30:10 -0500

    use properties for mongodb

--- src/main/java/com/hw/config/MongoConfig.java
+++ src/main/java/com/hw/config/MongoConfig.java
@@ -1,43 +1,23 @@
 package com.hw.config;
 
-import com.mongodb.ConnectionString;
-import com.mongodb.MongoClientSettings;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.MongoTransactionManager;
 import org.springframework.data.mongodb.SessionSynchronization;
-import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.transaction.support.TransactionTemplate;
 
 @Configuration
-public class MongoConfig extends AbstractMongoClientConfiguration {
+public class MongoConfig {
     @Bean
     MongoTransactionManager transactionManager(MongoDbFactory dbFactory) {
         return new MongoTransactionManager(dbFactory);
     }
 
     @Bean
-    public TransactionTemplate getTransactionTemplate(MongoTemplate mongoTemplate,MongoTransactionManager mongoTransactionManager) {
+    public TransactionTemplate getTransactionTemplate(MongoTemplate mongoTemplate, MongoTransactionManager mongoTransactionManager) {
         mongoTemplate.setSessionSynchronization(SessionSynchronization.ALWAYS);
         return new TransactionTemplate(mongoTransactionManager);
-    }
-
-    @Override
-    public MongoClient mongoClient() {
-        final ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017/test");
-        final MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
-                .applyConnectionString(connectionString)
-                .build();
-        return MongoClients.create(mongoClientSettings);
-    }
-
-    @Override
-    protected String getDatabaseName() {
-        return "test";
     }
 }
