064a841d4380aef5f391ae9f89cc2e142bb2f478
publicdevop2019 <publicdevop2019hw@gmail.com>
2021-01-03 15:35:49 -0500

    accept string id

--- src/main/java/com/hw/aggregate/event/BizEventController.java
+++ src/main/java/com/hw/aggregate/event/BizEventController.java
@@ -17,33 +17,65 @@ public class BizEventController {
     AdminBizEventApplicationService applicationService;
 
     @PostMapping("{id}")
-    public ResponseEntity<Void> create(@RequestHeader("authorization") String authorization, @RequestBody Object[] data, @PathVariable(name = "id") Long id, @RequestHeader(HTTP_HEADER_CHANGE_ID) String changeId) {
+    public ResponseEntity<Void> create(@RequestHeader("authorization") String authorization, @RequestBody Object[] data, @PathVariable(name = "id") String id, @RequestHeader(HTTP_HEADER_CHANGE_ID) String changeId) {
         UserThreadLocal.unset();
         UserThreadLocal.set(ServiceUtility.getUserId(authorization));
-        applicationService.create(data, id,changeId);
+        Long parsed;
+        try {
+            parsed = Long.parseLong(id);
+
+        } catch (Exception ex) {
+            String substring = id.substring(2);
+            parsed = Long.valueOf(substring, 36);
+        }
+        applicationService.create(data, parsed, changeId);
         return ResponseEntity.ok().header("Location", id.toString()).build();
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<AdminBizEventRep> read(@RequestHeader("authorization") String authorization, @PathVariable(value = "id") Long id) {
+    public ResponseEntity<AdminBizEventRep> read(@RequestHeader("authorization") String authorization, @PathVariable(value = "id") String id) {
         UserThreadLocal.unset();
         UserThreadLocal.set(ServiceUtility.getUserId(authorization));
-        return ResponseEntity.ok(applicationService.readById(id));
+        Long parsed;
+        try {
+            parsed = Long.parseLong(id);
+
+        } catch (Exception ex) {
+            String substring = id.substring(2);
+            parsed = Long.valueOf(substring, 36);
+        }
+        return ResponseEntity.ok(applicationService.readById(parsed));
     }
 
     @PutMapping("{id}")
-    public ResponseEntity<?> update(@RequestHeader("authorization") String authorization, @PathVariable(value = "id") Long id, @RequestBody AdminUpdateBizEventCommand command, @RequestHeader(HTTP_HEADER_CHANGE_ID) String changeId) {
+    public ResponseEntity<?> update(@RequestHeader("authorization") String authorization, @PathVariable(value = "id") String id, @RequestBody AdminUpdateBizEventCommand command, @RequestHeader(HTTP_HEADER_CHANGE_ID) String changeId) {
         UserThreadLocal.unset();
         UserThreadLocal.set(ServiceUtility.getUserId(authorization));
-        applicationService.update(id, command,changeId);
+        Long parsed;
+        try {
+            parsed = Long.parseLong(id);
+
+        } catch (Exception ex) {
+            String substring = id.substring(2);
+            parsed = Long.valueOf(substring, 36);
+        }
+        applicationService.update(parsed, command, changeId);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("{id}")
-    public ResponseEntity<?> delete(@RequestHeader("authorization") String authorization, @PathVariable(value = "id") Long id, @RequestHeader(HTTP_HEADER_CHANGE_ID) String changeId) {
+    public ResponseEntity<?> delete(@RequestHeader("authorization") String authorization, @PathVariable(value = "id") String id, @RequestHeader(HTTP_HEADER_CHANGE_ID) String changeId) {
         UserThreadLocal.unset();
         UserThreadLocal.set(ServiceUtility.getUserId(authorization));
-        applicationService.delete(id,changeId);
+        Long parsed;
+        try {
+            parsed = Long.parseLong(id);
+
+        } catch (Exception ex) {
+            String substring = id.substring(2);
+            parsed = Long.valueOf(substring, 36);
+        }
+        applicationService.delete(parsed, changeId);
         return ResponseEntity.ok().build();
     }
 }
